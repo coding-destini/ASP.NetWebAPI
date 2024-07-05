3eefef4a5a0d466d97a29a33805306e9ebafbf95
akash-shah-1 <akash.s@cisinlabs.com>
2024-07-05 17:04:26 +0530

    resolve migration error

--- Dtos/Stock/StockDto.cs
+++ Dtos/Stock/StockDto.cs
@@ -13,6 +13,6 @@ namespace api.Dtos.Stock
         public decimal LastDiv { get; set; }
         public string Industry { get; set; } = string.Empty;
         public long MarketCap { get; set; }
-        public List<CommentDto> Comments { get; set; }
+        public List<CommentDto>? Comments { get; set; }
     }
 }
